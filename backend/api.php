328f741a75b278711b415513bc8489d05556f787
joanago <j_gock02@uni-muenster.de>
2015-02-19 17:45:09 +0100

    Darstellung mein Kalender

--- backend/api.php
+++ backend/api.php
@@ -20,7 +20,7 @@
     $titles = explode(",", $ititles);
     $maxi = count($titles);
 	$help = $maxi;
-	$sql = "Select title, date, start, room_id
+	$sql = "Select title, date, start, room_id, duration, description
 		From Speech
 		Where title ";
     for ($i=0; $i <= $maxi; $i++)
@@ -49,6 +49,7 @@
       $test->start = (string)$row[2];
       $test->room = (string)$row[3];
 	  $test->duration = (string)$row[4];
+	  $test->description = (string)$row[5];
 	  $test->number = $number++;
 
       array_push($array, $test);
