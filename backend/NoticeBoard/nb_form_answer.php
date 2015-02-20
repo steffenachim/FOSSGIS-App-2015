32dcdeaad972bf0bf97889e1066012d0f8464856
PhilippStuewe <philipp.stuewe@uni-muenster.de>
2015-02-20 13:49:44 +0100

    Create nb_form_answer.php

---
+++ backend/NoticeBoard/nb_form_answer.php
@@ -0,0 +1,34 @@
+<?
+	/*
+		Answer form
+		
+	*/
+?>
+
+<script>
+    $(document).ready(function(){
+        // Bind to the submit event of our form
+        $("#answerform").submit(function(event){
+            $.post('nb_ret_insertcomment.php', $(this).serialize(),function(data) {
+                $("#commentInsert").html("Der Kommentar wurde erfolgreich hinzugef&uuml;gt.");
+				DisplayComments();
+            });
+            
+            
+            return false;
+        });
+    });
+    
+</script>
+
+<div id="commentInsert">
+ <form action="index.php" method="post" id="answerform">
+    
+	<input type="hidden" name="answer_to" id="answer_to" value="!answerid!" />
+    Name: </br><input type="text" id="author_name" name="author_name" required /></br></br>
+    Mail (optional): </br><input type="text" id="author_mail" name="author_mail" /></br></br>
+    Text: </br><textarea name="message" id="message" required/></textarea ></br></br>
+    <input id="submit" type="submit" class="button expand" value="Abschicken" />
+</form>
+<br><br>
+</div>
